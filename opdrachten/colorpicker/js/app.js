ef4cc1f07434d9e9cc19c57b95087f8e5d49ed9e
Cedricaf <34737@ma-web.nl>
2024-04-08 18:42:30 +0200

    mdofied

--- opdrachten/colorpicker/js/app.js
+++ opdrachten/colorpicker/js/app.js
@@ -1,7 +1,28 @@
-const color = document.getElementById("js--color--1");
+const color = document.getElementsByClassName("colors__color");
 
-console.log(color);
 
-color.onclick = function(){
-    color.children[0].classList.add("colors__circle--selected");
+
+for (let index = 0; index < color.length; index++) {
+
+
+    let randomHue = Math.floor(Math.random() *  (360 - 1) + 1);
+    let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
+    let randomLightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
+
+    color[index].children[0].style.background = `hsl(${randomHue}, ${randomSaturation}, ${randomLightness})`;
+
+    console.log(randomHue, randomSaturation, randomLightness);
+
+
+    
+
+
+    color[index].onclick = function(){
+        color[index].children[0].classList.add("colors__circle--selected");
+    	navigator.clipboard.writeText(color[index].children[0].style.background);
+        console.log(color[index].children[0])
+        document.title = color[index].children[0].style.background;
+        
+    }
 }
+
