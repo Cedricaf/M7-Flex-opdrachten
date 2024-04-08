385f7f0854e5eb830eb84f40c0c4ae965c118ada
Cedricaf <34737@ma-web.nl>
2024-04-08 21:51:10 +0200

    finished colorpicker

--- opdrachten/colorpicker/js/app.js
+++ opdrachten/colorpicker/js/app.js
@@ -1,16 +1,8 @@
-class ColorCard{
-    id;
-    color;
-    addToList;
-    htmlElement;
-    circle;
-    text
-
-    constructor(newId, newColor, addToList){
+class ColorCard {
+    constructor(newId, newColor, colorList) {
         this.id = newId;
         this.color = newColor;
-        this.addToList = addToList;
-
+        this.colorList = colorList;
 
         this.htmlElement = document.createElement("li");
         this.htmlElement.classList = "colors__color";
@@ -22,60 +14,78 @@ class ColorCard{
         this.text.innerText = "Copied";
         this.text.classList = "colors__text";
         this.htmlElement.appendChild(this.text);
-        this.htmlElement.onclick = this.onHTMLElementClicked;
+        this.htmlElement.onclick = this.onHTMLElementClicked.bind(this);
         this.render();
     }
 
-    onHTMLElementClicked = () => {
+    onHTMLElementClicked() {
         this.circle.classList.add("colors__circle--selected");
-        window.navigator.title = this.color;
-        window.navigator.clipboard.writeText(this.color);
+        document.title = this.color; // Set the document title
+        navigator.clipboard.writeText(this.color);
     }
 
-    render(){
-    
-        this.addToList.appendChild(this.htmlElement);
-    
+    render() {
+        this.colorList.htmlElement.appendChild(this.htmlElement);
     }
-
 }
 
-
-class ColorList{
-    id;
-    htmlElement;
-
-    constructor(newId){
+class ColorList {
+    constructor(newId) {
         this.id = newId;
         this.htmlElement = document.createElement("ul");
         this.htmlElement.id = this.id;
         this.htmlElement.classList.add("colors");
         this.render();
-
-
     }
 
-    render(){
+    render() {
         document.querySelector("body").appendChild(this.htmlElement);
     }
 }
 
-const colorList = new ColorList("js--colors");
+class HSLGenerator {
+    constructor() {
+        this.generateHSL();
+    }
 
-for (let i = 1; i < 101; i++) {
+    generateHue() {
+        this.randomHue = Math.floor(Math.random() * (360 - 1) + 1);
+    }
 
-    let randomHue = Math.floor(Math.random() *  (360 - 1) + 1);
-    let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
-    let randomLightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
+    generateSaturation() {
+        this.randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
+    }
 
-    let hsl = `hsl(${randomHue}, ${randomSaturation}, ${randomLightness})`;
+    generateLightness() {
+        this.randomLightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
+    }
 
-    new ColorCard(i, hsl, document.getElementById(colorList.id));
-    
+    generateHSL() {
+        this.generateHue();
+        this.generateSaturation();
+        this.generateLightness();
+        this.hsl = `hsl(${this.randomHue}, ${this.randomSaturation}, ${this.randomLightness})`;
+    }
 }
 
+class App {
+    constructor(newId) {
+        this.id = newId;
+        this.colorList = new ColorList("js--list");
+        this.hslGenerator = new HSLGenerator();
+        this.generateColorCards();
+    }
+
+    generateColorCards() {
+        for (let i = 1; i <= 100; i++) {
+            this.hslGenerator.generateHSL();
+            new ColorCard(i, this.hslGenerator.hsl, this.colorList);
+        }
+    }
+}
+
+const app = new App("js--app");
+const app2 = new App("js--app--1");
+const app3 = new App("js--app--2");
 
 
-
-
-console.log(window.navigator);
